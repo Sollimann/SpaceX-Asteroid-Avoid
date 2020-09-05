45fc9b2258c5519ff35ef2eaf0fb988da8e6f808
sollimann <kristoffer.solberg@cognite.com>
2020-09-05 21:14:35 +0200

    deprecated old game screen, and added a new game screen that instansiate controller and renderer in constructor instead

--- core/src/com/obstacleavoid/game/screen/game/GameScreen.kt
+++ core/src/com/obstacleavoid/game/screen/game/GameScreen.kt
@@ -3,31 +3,51 @@ package com.obstacleavoid.game.screen.game
 import com.badlogic.gdx.Screen
 
 class GameScreen : Screen {
-    override fun hide() {
-        TODO("Not yet implemented")
-    }
 
+    private lateinit var controller : GameController
+    private lateinit var renderer : GameRenderer
+
+    /** Called when this screen becomes the current screen for a {@link Game}. */
     override fun show() {
-        TODO("Not yet implemented")
+        controller = GameController()
+        renderer = GameRenderer(controller)
     }
 
+    /** Called when the screen should render itself.
+     * @param delta The time in seconds since the last render. */
     override fun render(delta: Float) {
-        TODO("Not yet implemented")
+        controller.update(delta)
+        renderer.render(delta)
     }
 
+    /** Called when the {@link Application} is paused, usually when it's not active or visible on-screen. An Application is also
+     * paused before it is destroyed. */
     override fun pause() {
         TODO("Not yet implemented")
     }
 
+    /** Called when the {@link Application} is resumed from a paused state, usually when it regains focus. */
     override fun resume() {
         TODO("Not yet implemented")
     }
 
+    /** Called when the {@link Application} is resized. This can happen at any point during a non-paused state but will never happen
+     * before a call to {@link #create()}.
+     *
+     * @param width the new width in pixels
+     * @param height the new height in pixels */
     override fun resize(width: Int, height: Int) {
-        TODO("Not yet implemented")
+        renderer.resize(width, height)
     }
 
+    /** Called when this screen should release all resources. */
     override fun dispose() {
-        TODO("Not yet implemented")
+        renderer.dispose()
     }
+    /** Called when this screen is no longer the current screen for a {@link Game}. */
+    override fun hide() {
+        // WARNING: Screens are not disposed automatically
+       dispose()
+    }
+
 }
